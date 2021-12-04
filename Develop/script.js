84a7b02d0d0802687ec3f2ff8a0c3c5138ad9f78
Adilene Bejines <adi3511@hotmail.com>
2021-12-04 13:54:27 -0700

    added a fuction to get the tasks from local storage

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,7 @@
 // display date assign return value
 var currentDayP = document.querySelector("#currentDay"); 
 
-var currentDate = moment().format("MMMM Do YYYY, h:mm:ss a");
+var currentDate = moment().format("MMMM Do YYYY");
 //console.log(currentDate);
 currentDayP.textContent = currentDate;
     
@@ -17,11 +17,20 @@ var tasks = {
     "16": [],
     "17": [],
 };
-/*this ass tasks to localStorage*/
+/*this adds tasks to localStorage*/
 var setTasks = function(){
     localStorage.setItem("tasks", JSON.stringify(tasks));
 }
 
-var auditTaks = function() {
-    var currentHour = moment().hour();
+var getTasks = function() {
+    var loadedTasks = JSON.parse(localStorage.getItem("tasks"));
+    if (loadedTasks) {
+        tasks = loadedTasks
+
+        $.each(tasks, function(hour, task) {
+            var hourDiv = $("#" + hour);
+            createTask(task, hourDiv);
+        })
+    }
+    auditTasks();
 }
