9c54770b5e7f42c3106b8746804fbba917619bf4
Adilene Bejines <adi3511@hotmail.com>
2021-12-05 20:32:00 -0700

    added last touches and added to read me

--- Develop/script.js
+++ Develop/script.js
@@ -1,7 +1,7 @@
-// display date assign return value
+// display date 
 var currentDayP = document.querySelector("#currentDay"); 
 
-var currentDate = moment().format("MMMM Do YYYY");
+var currentDate = moment().format("MMMM Do, YYYY");
 //console.log(currentDate);
 currentDayP.textContent = currentDate;
     
@@ -21,16 +21,83 @@ var tasks = {
 var setTasks = function(){
     localStorage.setItem("tasks", JSON.stringify(tasks));
 }
-
+// getting the tasks from the local storage
 var getTasks = function() {
     var loadedTasks = JSON.parse(localStorage.getItem("tasks"));
     if (loadedTasks) {
         tasks = loadedTasks
-
+        //with EACH key/value pair in tasks, creates a task
         $.each(tasks, function(hour, task) {
             var hourDiv = $("#" + hour);
             createTask(task, hourDiv);
         })
     }
-    auditTasks();
+    //auditTasks();
 }
+//creating the tasks
+var createTask = function(taskText, hourDiv) {
+    //were finding the .task in the HTML 
+    var taskDiv = hourDiv.find(".task");
+    //adding a <p> and adding text
+    var taskP = $("<p>").addClass().text(taskText)
+    taskDiv.html(taskP);
+}
+
+
+//so you can type in the boxes
+var replaceTextArea = function(textAreaElement) {
+
+    var taskInfo = textAreaElement.closest(".task-info");
+    var textArea = taskInfo.find("textArea");
+
+    var time = taskInfo.attr("id");
+    var text = textArea.val().trim();
+
+    tasks[time] = [text];
+    setTasks();
+
+    createTask(text, taskInfo);
+}
+//this was supposed to change the background
+function trackTime() {
+    
+    var timeNow = moment().hour();
+    //finding each .task-info
+    $(".task").each(function() {
+    var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+        //if the time for the block is less than the current time, its supposed to get the color for the pasr
+        if (blockTime < timeNow) {
+            $(".task").addClass("past");
+        // if both times are equal to eachother, then the color was supposed to change to the color for the present 
+        } else if (blockTime === timeNow) {
+            $(".task").addClass("present");
+        //else, it gets the color for the future
+        } else {
+            $(".task").addClass("future");
+        }
+    })
+}
+//the tasks
+$(".task").click(function(){
+    $("textArea").each(function(){
+        replaceTextArea($(this));
+    })
+
+    var time = $(this).closest(".task-info").attr("id");
+    if (parseInt(time) >= moment().hour()) {
+
+        var text = $(this).text();
+        var textInput = $("<textArea>").addClass("form-control").val(text);
+
+        $(this).html(textInput);
+        textInput.trigger("focus");
+
+    }
+})
+
+// the save button
+$(".saveBtn").click(function() {
+    replaceTextArea($(this));
+})
+
+getTasks();
--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# Work Day Scheduler Starter Code
+We were tasked to make a workday scheduler that displays the current date at the top of the page, displays the time blocks for the standard business hours(9AM-5PM), color coded time blocks depending if it's in the past, present, or future, be able to enter and save the event. Must also be able to save the event even if the page is refreshed.
+
+This week, I struggled on balancing homework and my life so I did not focus on styling the page. I wanted to make sure that it was functional. I was able to accomplish almost all the tasks besides the background changing depending on the time. I really struggled finding out how to use moment.js but I was happy to at least have the date displayed. 
+
+I will most likely make this page better once I am better at JavaScript. 
